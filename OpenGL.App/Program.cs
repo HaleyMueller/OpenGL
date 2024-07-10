8f3bc94700d0013bd5d73ed6b698d8fe8bf48b45
Haley Mueller <1998haleymueller@gmail.com>
2024-07-10 17:14:38 -0500

    Updating System.Drawing.Common

--- OpenGL.App/Program.cs
+++ OpenGL.App/Program.cs
@@ -1,12 +1,4 @@
 ﻿using System;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using OpenTK;
-using OpenTK.Graphics;
-using OpenTK.Graphics.OpenGL;
-using OpenTK.Windowing.Desktop;
 
 namespace OpenGL.App
 {
